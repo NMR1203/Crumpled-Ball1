b8aa36ea91cce299ce794d278e5edaac33cfc7a3
NMR1203 <72468607+NMR1203@users.noreply.github.com>
2020-11-06 12:59:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+//changed the controls so that you could move left or right with their respective arrow keys
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var dustbin, ball, ground;
+var world;
+
+function setup(){
+	createCanvas(1600,700);
+	rectMode(CENTER)
+	
+	engine = Engine.create();
+	world = engine.world;
+	
+	dustbin = new Dustbin(1200,650);
+	
+	ball = new Paper(200,450,40);
+	
+	ground = new Ground(width/2,670,width,20);
+
+
+	Engine.run(engine);
+}
+function draw(){
+	rectMode(CENTER);
+	background(0);
+	
+	dustbin.display();
+	ball.display();
+	ground.display();
+
+}
+
+function keyPressed(){
+	if(keyCode === RIGHT_ARROW){
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85});	
+	}
+	if(keyCode === LEFT_ARROW){
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:-85,y:-85});
+	}
+} 
